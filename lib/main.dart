2152df178037c0b28ad06b4572760c35cdbc1d04
kjennn <gva05037@gmail.com>
2022-01-13 22:23:55 +0900

    my_app toy project
    내장 이미지 업로드, 함수 사용
    플러터는 트리 구조이다. build는 거대한 중첩 표현식 하나를 return.
    
    이미지 출처 : https://cafe.naver.com/steamindiegame/4276694

--- lib/main.dart
+++ lib/main.dart
@@ -63,53 +63,37 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+
+    return Image.asset('assets/images/2.png', fit: BoxFit.cover,);
+
+    // return Person( firstName : "Sarah", lastName : "Ali");  // 함수 사용
+    // return  MaterialApp(                                    // 트리 구조
+    //   home: Scaffold(
+    //     appBar: AppBar(
+    //       title: Text("A fancier app"),
+    //     ),
+    //     body: Container(
+    //       alignment: Alignment.center,
+    //       child: Text("Hello World"),
+    //     ),
+    //     floatingActionButton: FloatingActionButton(
+    //       child:  Icon(Icons.thumb_up),
+    //       onPressed: () => {},
+    //     ),
+    //   ),
+    // );
+  }
+}
+
+class Person extends StatelessWidget {
+  final String firstName;
+  final String lastName;
+  Person({required this.firstName, required this.lastName}) {}
+  Widget build(BuildContext context){
+    return Container(
+      child: Text( '$firstName $lastName'),
+
+
+    ) ;
   }
 }
